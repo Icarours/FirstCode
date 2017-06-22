88ad36bb93d85d78472ba6f1fcc9e22791006f16
j376787348 <j376787348@163.com>
2017-06-22 13:48:58 +0800

    1.添加一个测试用TestActivity,想做出Activity全屏的效果,但是百度出来的回答没有什么用

---
+++ weatherforecast/src/main/java/com/syl/weatherforecast/activity/Main2Activity.java
@@ -0,0 +1,21 @@
+package com.syl.weatherforecast.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.syl.weatherforecast.R;
+
+/**
+ * author   Bright
+ * date     2017/6/22 13:45
+ * desc
+ * 测试用Activity
+ */
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
